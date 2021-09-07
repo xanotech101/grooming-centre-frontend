a525441a9bcecc766c48d2b5347fefe3d8474ae3
RichCode <chineduogada@gmail.com>
2021-09-07 23:00:00 +0100

    chore(DatePicker): config for use
    [#1]

--- src/App/Providers.jsx
+++ src/App/Providers.jsx
@@ -1,7 +1,15 @@
 import { ChakraProvider } from "@chakra-ui/react";
+import DateFnsUtils from "@date-io/date-fns";
+import { MuiPickersUtilsProvider } from "@material-ui/pickers";
 import theme from "../theme/theme";
 
 function Providers({ children }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
+  return (
+    <ChakraProvider theme={theme}>
+      <MuiPickersUtilsProvider utils={DateFnsUtils}>
+        {children}
+      </MuiPickersUtilsProvider>
+    </ChakraProvider>
+  );
 }
 export default Providers;
