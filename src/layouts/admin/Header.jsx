f6d8cf3400b32665d097cafb86f1d3b7e8a45cc5
RichCode <chineduogada@gmail.com>
2021-09-07 17:16:59 +0100

    refactor(theme): add the brand's theme
    [#1]

--- src/layouts/admin/Header.jsx
+++ src/layouts/admin/Header.jsx
@@ -2,7 +2,7 @@ import { Box } from "@chakra-ui/layout";
 import React from "react";
 
 const Header = () => {
-  return <Box as="header" height="61px" backgroundColor="#69192D"></Box>;
+  return <Box as="header" height="61px" backgroundColor="primary.base"></Box>;
 };
 
 export default Header;
