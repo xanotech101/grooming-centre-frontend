78eb45ae73771957168bbcea159a8c279b5e1a48
RichCode <chineduogada@gmail.com>
2021-09-13 15:59:46 +0100

    refactor(CoursesGridLayout): extract template to component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,3 +1,4 @@
 export * from "./admin/CreatePageLayout";
+export * from "./user/Courses/CoursesGridLayout";
 export * from "./user/OnBoardingLayout/FormLayout";
 export * from "./user/OnBoardingLayout/OnBoardingLayout";
---
+++ src/layouts/user/Courses/CoursesGridLayout.jsx
@@ -0,0 +1,30 @@
+import { Grid } from "@chakra-ui/layout";
+import { CourseBoxCard } from "../../../components";
+import { courses } from "../../../pages/user/Courses/fakeCourses";
+
+export const CoursesGridLayout = () => {
+  return (
+    <Grid
+      templateColumns={{
+        base: "repeat(1, 1fr)",
+        "mobile-m": "repeat(2, 250px)",
+        "mobile-l": "repeat(2, 1fr)",
+        tablet: "repeat(3, 1fr)",
+        laptop: "repeat(4, 1fr)",
+        "laptop-l": "repeat(5, 1fr)",
+        "4k": "repeat(6, 1fr)",
+      }}
+      overflowX={{
+        base: "hidden",
+        "mobile-m": "scroll",
+        "mobile-l": "hidden",
+      }}
+      overflowY="hidden"
+      gap={{ base: 2, laptop: 5 }}
+    >
+      {courses.map((course, index) => (
+        <CourseBoxCard key={index} {...course} />
+      ))}
+    </Grid>
+  );
+};
