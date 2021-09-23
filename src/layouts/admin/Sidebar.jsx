50b91af3c59bc221ab110d2b3a64e1313965191c
RichCode <chineduogada@gmail.com>
2021-09-23 12:36:12 +0100

    feature(AdminLayout): add icons to Sidebar

--- src/layouts/admin/Sidebar.jsx
+++ src/layouts/admin/Sidebar.jsx
@@ -1,5 +1,10 @@
+import Icon from "@chakra-ui/icon";
 import { Box, Flex, Stack } from "@chakra-ui/layout";
 import { Skeleton } from "@chakra-ui/skeleton";
+import { FiSettings } from "react-icons/fi";
+import { GiBookshelf } from "react-icons/gi";
+import { HiUsers } from "react-icons/hi";
+import { RiDashboardLine } from "react-icons/ri";
 import { NavLink } from "react-router-dom";
 import { Text } from "../../components";
 import colors from "../../theme/colors";
@@ -7,25 +12,63 @@ import colors from "../../theme/colors";
 const links = [
   {
     to: "/admin/",
-    children: "Dashboard",
+    text: "Dashboard",
     exact: true,
+    icon: <RiDashboardLine />,
   },
   {
     // to: "/admin/users",
     to: "/admin/users/create",
-    children: "Users",
+    text: "Users",
+    icon: <HiUsers />,
   },
   {
     to: "/admin/courses",
-    children: "Courses",
+    text: "Courses",
+    icon: <GiBookshelf />,
   },
   {
-    to: "/admin/roles",
-    children: "Roles",
-  },
-  {
-    to: "/admin/settings",
-    children: "Settings",
+    to: "/admin/manage/users",
+    text: "Manage",
+    icon: <FiSettings />,
+    links: [
+      {
+        to: "/admin/manage/users",
+        text: "Manage Users",
+      },
+      {
+        to: "/admin/manage/add-course",
+        text: "Add New Course",
+      },
+      {
+        to: "/admin/manage/add-lesson",
+        text: "Add New Lesson",
+      },
+      {
+        to: "/admin/manage/add-assessment",
+        text: "Add New Assessment",
+      },
+      {
+        to: "/admin/manage/library",
+        text: "Manage Library",
+      },
+      {
+        to: "/admin/manage/events",
+        text: "Manage Events",
+      },
+      {
+        to: "/admin/manage/create-quiz",
+        text: "Create Quiz",
+      },
+      {
+        to: "/admin/manage/create-polls",
+        text: "Create Polls",
+      },
+      {
+        to: "/admin/manage/add-examination",
+        text: "Add Examination",
+      },
+    ],
   },
 ];
 
@@ -57,21 +100,29 @@ const Sidebar = () => {
 
         <nav>
           <Stack as="ul" spacing={2} listStyleType="none">
-            {links.map((linkProps, index) => (
-              <li key={index}>
+            {links.map((link) => (
+              <li key={link.text}>
                 <NavLink
-                  {...linkProps}
+                  to={link.to}
+                  exact={link.exact}
                   style={{
-                    display: "grid",
-                    placeItems: "center",
-                    height: "40px",
-                    borderRadius: "5px",
+                    display: "block",
+                    borderRadius: "4px",
+                    color: colors.accent[3],
                   }}
                   activeStyle={{
                     backgroundColor: colors.primary.base,
                     color: "white",
                   }}
-                />
+                >
+                  <Flex alignItems="center" paddingY={3} paddingX={3}>
+                    <Icon fontSize="heading.h3" marginRight={3}>
+                      {link.icon}
+                    </Icon>
+
+                    {link.text}
+                  </Flex>
+                </NavLink>
               </li>
             ))}
           </Stack>
