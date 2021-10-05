a2146184e93c53a1467797d42db0d6f6f0f3473c
RichCode <chineduogada@gmail.com>
2021-10-05 11:58:00 +0100

    chore(endpoints): mock user course listing
    [Start #26]

--- src/mocks/server/controllers/course/responses.js
+++ src/mocks/server/controllers/course/responses.js
@@ -22,6 +22,7 @@ export const adminCourseListingRes = {
     },
   ],
 };
+
 export const userCourseListingRes = {
   data: [
     {
@@ -30,13 +31,14 @@ export const userCourseListingRes = {
       description: "Learn HTML and CSS for free today",
       disabled: true,
       title: "Web dev 2021",
-      thumbnail: "https://images.unsplash.com/photo-1611162616475-46b635cb6868",
+      thumbnail:
+        "https://images.unsplash.com/photo-1520607162513-77705c0f0d4a?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=869&q=80",
       // departmentId: "departmentId_1",
       lessonCount: 0,
       instructor: {
         id: "instructorId_1",
         profilePics:
-          "https://images.unsplash.com/photo-1611162616475-46b635cb6868",
+          "https://images.unsplash.com/flagged/photo-1570612861542-284f4c12e75f?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1740&q=80",
         firstName: "john",
         lastName: "doe",
       },
@@ -48,13 +50,14 @@ export const userCourseListingRes = {
       description: "Nodejs Advanced Concept",
       disabled: false,
       title: "Backend Development",
-      thumbnail: "https://images.unsplash.com/photo-1611162616475-46b635cb6868",
+      thumbnail:
+        "https://images.unsplash.com/photo-1578574577315-3fbeb0cecdc2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=872&q=80",
       // departmentId: "departmentId_2",
       lessonCount: 24,
       instructor: {
         id: "instructorId_2",
         profilePics:
-          "https://images.unsplash.com/photo-1611162616475-46b635cb6868",
+          "https://images.unsplash.com/photo-1438761681033-6461ffad8d80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=870&q=80",
         firstName: "jane",
         lastName: "bar",
       },
