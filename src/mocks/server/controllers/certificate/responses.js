4205e5b76d44f037366ea93344785ad77018202f
vicreative <vogbiyoyo@gmail.com>
2021-10-12 11:05:17 +0100

    ft(course-overview-page): integrate `backend service` for certificate

---
+++ src/mocks/server/controllers/certificate/responses.js
@@ -0,0 +1,26 @@
+export const requestCertificateDetailsRes_CourseId_1 = {
+  data: {
+    courseId: "courseId_1",
+    title: "Web Design & Development Crash Course 2021",
+    name: "Stanley Chinedu",
+    endDate: "May 2021",
+  },
+};
+
+export const requestCertificateDetailsRes_CourseId_2 = {
+  data: {
+    courseId: "courseId_2",
+    title: "Web Design & Development Crash Course 2021",
+    name: "Stanley Chinedu",
+    endDate: "April 2021",
+  },
+};
+
+export const requestCertificateDetailsRes_CourseId_3 = {
+  data: {
+    courseId: "courseId_3",
+    title: "Web Design & Development Crash Course 2021",
+    name: "Stanley Chinedu",
+    endDate: "June 2021",
+  },
+};
