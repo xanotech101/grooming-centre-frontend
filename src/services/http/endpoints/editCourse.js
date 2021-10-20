a53bc954331f0049d6ca0ade754782a723d878cf
vicreative <vogbiyoyo@gmail.com>
2021-10-20 10:02:05 +0100

    ft(edit-course-info-page): create new file for `adminEditCourse` endpoint

---
+++ src/services/http/endpoints/editCourse.js
@@ -0,0 +1,19 @@
+import { http } from "../http";
+
+/**
+ * Endpoint for course editing/modification
+ * @param {string} courseId
+ * @param {object} body
+ * @returns {Promise<{ message: string, data: Course }>}
+ */
+export const adminEditCourse = async (courseId, body) => {
+  const path = `/course/edit/${courseId}`;
+
+  const {
+    data: { message, data },
+  } = await http.post(path, body);
+
+console.log(data);
+
+  return { message, data: data[0] };
+};
