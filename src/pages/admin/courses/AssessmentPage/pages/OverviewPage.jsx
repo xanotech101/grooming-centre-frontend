6df8a8eac2ed9b19c60f6a9bbee31da23d08afe8
vicreative <vogbiyoyo@gmail.com>
2021-10-27 13:41:06 +0100

    ft(create-assessment): use DateTimePicker Component for startdate & time

--- src/pages/admin/courses/AssessmentPage/pages/OverviewPage.jsx
+++ src/pages/admin/courses/AssessmentPage/pages/OverviewPage.jsx
@@ -1,17 +1,17 @@
 import { Box, Flex, Grid, GridItem } from "@chakra-ui/layout";
 import { useForm } from "react-hook-form";
 import { Route } from "react-router-dom";
-import { Button, DatePicker, Input, Text } from "../../../../../components";
-import { useGoBack } from "../../../../../hooks";
+import { Button, DateTimePicker, Input } from "../../../../../components";
+import { useGoBack, useQueryParams } from "../../../../../hooks";
 import { AdminMainAreaWrapper } from "../../../../../layouts";
 
 const OverviewPage = () => {
-
+const isExamination = useQueryParams().get("examination");
    const {
      register,
      handleSubmit,
      formState: { errors },
-  } = useForm();
+   } = useForm();
   
   const handleCancel = useGoBack();
 
@@ -21,53 +21,58 @@ const OverviewPage = () => {
 
   return (
     <AdminMainAreaWrapper>
-      <Box
-        as="form"
-        backgroundColor="white"
-        onSubmit={handleSubmit(onSubmit)}
-        marginY={14}
-        marginX={6}
-        padding={10}
-      >
-        <Input
-          label="Assessment Title"
-          id="title"
-          {...register("title", {
-            required: "Title is required",
-          })}
-        />
-        {errors.title ? (
-          <Text color="secondary.5" style={{ marginTop: 0 }}>
-            {errors.title.message}
-          </Text>
-        ) : null}
-        <Grid templateColumns="repeat(2, 1fr)" gap={10} marginY={10}>
-          <GridItem>
-            <DatePicker id="date" isRequired label="Start date & time" />
-          </GridItem>
-          <GridItem>
-            <Input
-              label="Duration"
-              id="duration"
-              placeholder="Enter duration in minutes"
-              {...register("duration", {
-                required: "Please enter duration",
-              })}
+      <Box as="form" onSubmit={handleSubmit(onSubmit)} marginY={14} marginX={6}>
+        <Box backgroundColor="white" padding={10}>
+          <Input
+            label={isExamination ? "Examination Title" : "Assessment Title"}
+            id="title"
+            error={errors.title?.message}
+            {...register("title", {
+              required: "Title is required",
+            })}
+          />
+          <Grid templateColumns="repeat(2, 1fr)" gap={10} marginY={10}>
+            <GridItem>
+            <DateTimePicker
+              id="startTime"
+              // isRequired
+              label="Start date & time"
+              // value={startTimeManager.value}
+              // onChange={startTimeManager.handleChange}
             />
-            {errors.duration ? (
-              <Text color="secondary.5" style={{ marginTop: 0 }}>
-                {errors.duration.message}
-              </Text>
-            ) : null}
           </GridItem>
-        </Grid>
-      </Box>
-      <Flex paddingY={10} marginX={6} justifyContent="space-between">
+            <GridItem>
+              <Input
+                label="Duration"
+                id="duration"
+                placeholder="Enter duration in minutes"
+                error={errors.duration?.message}
+                {...register("duration", {
+                  required: "Please enter duration",
+                })}
+              />
+            </GridItem>
+            <GridItem>
+              <Input
+                label="Number of Questions"
+                id="numberOfQuestions"
+                placeholder="Enter number of questions"
+                error={errors.numberOfQuestions?.message}
+                {...register("numberOfQuestions", {
+                  required: "Please enter number of questions",
+                })}
+              />
+            </GridItem>
+          </Grid>
+        </Box>
+        <Flex paddingY={10} marginX={6} justifyContent="space-between">
         <Button secondary onClick={handleCancel}>
           Cancel
         </Button>
         <Button type="submit">Save</Button>
       </Flex>
+      </Box>
+      
     </AdminMainAreaWrapper>
   );
 };
