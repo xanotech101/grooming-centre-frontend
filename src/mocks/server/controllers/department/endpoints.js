c0819056f4e763994630f4f406a08ba108ccb552
vicreative <vogbiyoyo@gmail.com>
2021-11-09 15:38:05 +0100

    ft(view-department-page): integrate the backend service

--- src/mocks/server/controllers/department/endpoints.js
+++ src/mocks/server/controllers/department/endpoints.js
@@ -1,8 +1,13 @@
 import { rest } from "msw";
 import { getUrl } from "../../http";
 import { handleSuccessResponse } from "../helpers";
-import { adminCreateDepartmentRes, adminGetDepartmentListingRes } from "./responses";
-
+import {
+  adminCreateDepartmentRes,
+  adminGetDepartmentListingRes,
+  adminGetDepartmentUsersListingRes_departmentId_1,
+  adminGetDepartmentUsersListingRes_departmentId_2,
+  adminGetDepartmentUsersListingRes_departmentId_3,
+} from "./responses";
 
 const adminGetDepartmentListing = rest.get(
   getUrl("/department/all"),
@@ -14,7 +19,25 @@ const adminCreateDepartment = rest.post(
   handleSuccessResponse(adminCreateDepartmentRes)
 );
 
-const department = [adminGetDepartmentListing, adminCreateDepartment];
+const adminGetDepartmentUsersListing = [
+  rest.get(
+    getUrl("department/users/departmentId_1"),
+    handleSuccessResponse(adminGetDepartmentUsersListingRes_departmentId_1)
+  ),
+  rest.get(
+    getUrl("department/users/departmentId_2"),
+    handleSuccessResponse(adminGetDepartmentUsersListingRes_departmentId_2)
+  ),
+  rest.get(
+    getUrl("department/users/departmentId_3"),
+    handleSuccessResponse(adminGetDepartmentUsersListingRes_departmentId_3)
+  ),
+];
 
+const department = [
+  adminGetDepartmentListing,
+  adminCreateDepartment,
+  ...adminGetDepartmentUsersListing,
+];
 
 export default department;
--- src/mocks/server/controllers/department/responses.js
+++ src/mocks/server/controllers/department/responses.js
@@ -3,28 +3,28 @@ export const adminGetDepartmentListingRes = {
   data: {
     rows: [
       {
-        id: "departmentId_1",
-        name: "Software development",
         active: true,
-        createdAt: "2021-10-13T09:44:42.008Z",
-        updatedAt: "2021-10-13T09:44:42.008Z",
+        createdAt: "2021-09-17T12:48:01.319Z",
+        id: "departmentId_1",
+        name: "Accounting",
+        updatedAt: "2021-09-17T12:48:01.319Z",
         noOfusers: 1,
       },
       {
-        id: "departmentId_2",
-        name: "Accounting",
         active: true,
-        createdAt: "2021-10-13T09:44:42.008Z",
-        updatedAt: "2021-10-13T09:44:42.008Z",
+        createdAt: "2021-09-17T12:48:01.319Z",
+        id: "departmentId_2",
+        name: "Software development",
+        updatedAt: "2021-09-17T12:48:01.319Z",
         noOfusers: 2,
       },
       {
-        id: "departmentId_3",
-        name: "computer science",
         active: true,
-        createdAt: "2021-10-18T13:01:29.695Z",
-        updatedAt: "2021-10-18T13:01:29.695Z",
-        noOfusers: 0,
+        createdAt: "2021-09-20T10:29:27.158Z",
+        id: "departmentId_3",
+        name: "Computer science",
+        updatedAt: "2021-09-20T10:29:27.158Z",
+        noOfusers: 2,
       },
     ],
   },
@@ -34,5 +34,89 @@ export const adminCreateDepartmentRes = {
   message: "Department created successfully",
   data: {
     id: "departmentId_1",
+  },
+};
+
+export const adminGetDepartmentUsersListingRes_departmentId_1 = {
+  data: {
+    count: 2,
+    rows: [
+      {
+        id: "userId_1",
+        firstName: "Sonia",
+        lastName: "Johnson",
+        email: "thebigolayinka@gmail.com",
+        userRoleId: "userRoleId_1",
+        departmentId: "departmentId_1",
+        userRole: {
+          id: "userRoleId_1",
+          name: "staff",
+        },
+      },
+    ],
+  },
+};
+
+export const adminGetDepartmentUsersListingRes_departmentId_2 = {
+  data: {
+    count: 2,
+    rows: [
+      {
+        id: "userId_1",
+        firstName: "Sarah",
+        lastName: "Gabriel",
+        email: "thebigolayinka@gmail.com",
+        userRoleId: "userRoleId_2",
+        departmentId: "departmentId_2",
+        userRole: {
+          id: "userRoleId_2",
+          name: "user",
+        },
+      },
+      {
+        id: "userId_2",
+        firstName: "Shalom",
+        lastName: "Brain",
+        email: "omolola@gmail.com",
+        userRoleId: "userRoleId_2",
+        departmentId: "departmentId_2",
+        userRole: {
+          id: "userRoleId_2",
+          name: "staff",
+        },
+      },
+    ],
+  },
+};
+
+export const adminGetDepartmentUsersListingRes_departmentId_3 = {
+  data: {
+    count: 2,
+    rows: [
+      {
+        id: "userId_1",
+        firstName: "Yinka",
+        lastName: "Joseph",
+        email: "thebigolayinka@gmail.com",
+        userRoleId: "userRoleId_3",
+        departmentId: "departmentId_3",
+        userRole: {
+          id: "userRoleId_3",
+          name: "user",
+        },
+      },
+      {
+        id: "userId_2",
+        firstName: "omolola",
+        lastName: "olaosebiikan",
+        email: "omolola@gmail.com",
+        userRoleId: "userRoleId_3",
+        departmentId: "departmentId_3",
+        userRole: {
+          id: "userRoleId_3",
+          name: "staff",
+        },
+      },
+    ],
   },
 };
