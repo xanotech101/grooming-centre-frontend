1798bba47378cdc926aa93799aa2eb1173ed16df
vicreative <vogbiyoyo@gmail.com>
2021-12-03 13:09:34 +0100

    resolve merge conflict

--- src/hooks/index.js
+++ src/hooks/index.js
@@ -12,8 +12,5 @@ export * from "./useUpload";
 export * from "./useDateTimePicker";
 export * from "./useRichText";
 export * from "./useDragAndDrop";
-<<<<<<< HEAD
 export * from "./useDownload";
-=======
 export * from "./useTableRows";
->>>>>>> 2b03ec82f84d70b31909ec615ad9b355d6ad550b
