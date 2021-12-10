63a1a5a543171c2387b3f921afec5a5178a9308c
vicreative <vogbiyoyo@gmail.com>
2021-12-10 16:07:31 +0100

    ch: fetch departments from metadata

--- src/pages/admin/courses/CourseListingPage/CourseListingPage.jsx
+++ src/pages/admin/courses/CourseListingPage/CourseListingPage.jsx
@@ -18,126 +18,131 @@ import { Tag } from "@chakra-ui/tag";
 import { BreadcrumbItem } from "@chakra-ui/react";
 import dayjs from "dayjs";
 import { useTableRows } from "../../../../hooks";
-
-const tableProps = {
-  filterControls: [
-    {
-      triggerText: "Department",
-      queryKey: "department",
-      width: "125%",
-      body: {
-        checks: [
-          { label: "Finance", queryValue: "finance" },
-          { label: "Engineering", queryValue: "engineering" },
-          {
-            label: "Accounting",
-            queryValue: "accounting",
-          },
-        ],
-      },
-    },
-    {
-      triggerText: "Sort",
-      queryKey: "sort",
-      triggerIcon: <FaSortAmountUpAlt />,
-      width: "200px",
-      position: "right-bottom",
-      // noFilterTags: true,
-      body: {
-        radios: [
-          {
-            label: "Alphabetically: ascending",
-            queryValue: "asc",
-            additionalParams: { date: false },
-          },
-          {
-            label: "Alphabetically: descending",
-            queryValue: "desc",
-            additionalParams: { date: false },
-          },
-          {
-            label: "Date: ascending",
-            queryValue: "asc",
-            additionalParams: { date: true },
-          },
-          {
-            label: "Date: descending",
-            queryValue: "desc",
-            additionalParams: { date: true },
-          },
-        ],
-      },
-    },
-  ],
-
-  columns: [
-    {
-      id: "1",
-      key: "title",
-      text: "Course Title",
-      fraction: "3fr",
-      renderContent: (data) => (
-        <Link href={`/admin/courses/details/${data.courseId}/info`}>
-          <Text>{data.text}</Text>
-        </Link>
-      ),
-    },
-    {
-      id: "3",
-      key: "instructor",
-      text: "Instructor",
-      fraction: "100px",
-    },
-    {
-      id: "4",
-      key: "startDate",
-      text: "Start Date",
-      fraction: "200px",
-    },
-    {
-      id: "5",
-      key: "status",
-      text: "Status",
-      fraction: "130px",
-      renderContent: (status) => (
-        <Box>
-          <Tag
-            borderRadius="full"
-            size="sm"
-            backgroundColor={status ? "accent.4" : "accent.1"}
-            color={status ? "accent.5" : "accent.3"}
-          >
-            <Text bold>{status ? "Published" : "UnPublished"}</Text>
-          </Tag>
-        </Box>
-      ),
-    },
-  ],
-
-  options: {
-    action: [
-      {
-        text: "View",
-        link: (course) => `/admin/courses/details/${course.id}/info`,
-      },
-      {
-        text: "Edit",
-        link: (course) => `/admin/courses/edit/${course.id}`,
-      },
-      {
-        isDelete: true,
-      },
-    ],
-    selection: true,
-    multipleDeleteFetcher: async (selectedCourses) => {
-      console.log(selectedCourses);
-      await adminDeleteMultipleCourses();
-    },
-    pagination: true,
-  },
-};
+import { useApp } from "../../../../contexts";
 
 const CourseListingPage = () => {
+  const appManager = useApp();
+
+  const departmentName = appManager.state.metadata?.departments.map(
+    (department) => department.name
+  );
+
+  const tableProps = {
+    filterControls: [
+      {
+        triggerText: "Department",
+        queryKey: "department",
+        width: "125%",
+        body: {
+          checks: [
+            ...(departmentName?.map((name) => ({
+              label: name,
+              queryValue: name,
+            })) || []),
+          ],
+        },
+      },
+      {
+        triggerText: "Sort",
+        queryKey: "sort",
+        triggerIcon: <FaSortAmountUpAlt />,
+        width: "200px",
+        position: "right-bottom",
+        // noFilterTags: true,
+        body: {
+          radios: [
+            {
+              label: "Alphabetically: ascending",
+              queryValue: "asc",
+              additionalParams: { date: false },
+            },
+            {
+              label: "Alphabetically: descending",
+              queryValue: "desc",
+              additionalParams: { date: false },
+            },
+            {
+              label: "Date: ascending",
+              queryValue: "asc",
+              additionalParams: { date: true },
+            },
+            {
+              label: "Date: descending",
+              queryValue: "desc",
+              additionalParams: { date: true },
+            },
+          ],
+        },
+      },
+    ],
+
+    columns: [
+      {
+        id: "1",
+        key: "title",
+        text: "Course Title",
+        fraction: "3fr",
+        renderContent: (data) => (
+          <Link href={`/admin/courses/details/${data.courseId}/info`}>
+            <Text>{data.text}</Text>
+          </Link>
+        ),
+      },
+      {
+        id: "3",
+        key: "instructor",
+        text: "Instructor",
+        fraction: "100px",
+      },
+      {
+        id: "4",
+        key: "startDate",
+        text: "Start Date",
+        fraction: "200px",
+      },
+      {
+        id: "5",
+        key: "status",
+        text: "Status",
+        fraction: "130px",
+        renderContent: (status) => (
+          <Box>
+            <Tag
+              borderRadius="full"
+              size="sm"
+              backgroundColor={status ? "accent.4" : "accent.1"}
+              color={status ? "accent.5" : "accent.3"}
+            >
+              <Text bold>{status ? "Published" : "UnPublished"}</Text>
+            </Tag>
+          </Box>
+        ),
+      },
+    ],
+
+    options: {
+      action: [
+        {
+          text: "View",
+          link: (course) => `/admin/courses/details/${course.id}/info`,
+        },
+        {
+          text: "Edit",
+          link: (course) => `/admin/courses/edit/${course.id}`,
+        },
+        {
+          isDelete: true,
+        },
+      ],
+      selection: true,
+      multipleDeleteFetcher: async (selectedCourses) => {
+        console.log(selectedCourses);
+        await adminDeleteMultipleCourses();
+      },
+      pagination: true,
+    },
+  };
+
   const mapCourseToRow = (course) => ({
     id: course.id,
     title: { text: course.title, courseId: course.id },
