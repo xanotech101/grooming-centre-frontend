7ea14859e317dafe993919ee5c86da317d3e7d41
RichCode <chineduogada@gmail.com>
2021-12-19 22:33:22 +0100

    feature(Chats): complete layout
    [Delivers #254]

--- src/layouts/user/Chat/ChatLayout.jsx
+++ src/layouts/user/Chat/ChatLayout.jsx
@@ -1,9 +1,15 @@
-import { Box, Grid } from "@chakra-ui/react";
+import { Avatar, Box, ButtonGroup, Flex, Grid } from "@chakra-ui/react";
 import { Route } from "react-router-dom";
-import { Heading } from "../../../components";
+import { Button, Heading, Text, Input } from "../../../components";
+import imagePlaceholder from "../../../assets/images/Avatar.svg";
 import breakpoints, {
   maxWidthStyles_userPages,
 } from "../../../theme/breakpoints";
+import { MdAttachFile } from "react-icons/md";
+import { GrEmoji } from "react-icons/gr";
+import { BiDotsVerticalRounded } from "react-icons/bi";
+import { AiOutlinePlus } from "react-icons/ai";
+import { IoIosSend } from "react-icons/io";
 
 const ChatLayout = () => {
   return (
@@ -14,20 +20,76 @@ const ChatLayout = () => {
       gridGap={10}
       padding={8}
     >
-      <Box as="aside">
+      <Box as="aside" shadow="sm">
         <Box background="accent.1" padding={3} roundedTop="md">
           <Heading fontSize="text.level3">Messaging</Heading>
         </Box>
       </Box>
 
-      <Box as="main">
-        <Box background="accent.1" padding={5} roundedTop="md">
-          <Heading fontSize="text.level3">Messaging</Heading>
-        </Box>
+      <Box as="main" shadow="sm">
+        <Flex
+          justifyContent="space-between"
+          background="accent.1"
+          padding={4}
+          roundedTop="md"
+        >
+          <Flex>
+            <Avatar
+              name="John doe"
+              src={imagePlaceholder}
+              boxSize="50px"
+              rounded="full"
+            />
+
+            <Box marginLeft={2}>
+              <Text bold>John doe</Text>
+              <Text color="accent.7">last online 5 hours ago</Text>
+            </Box>
+          </Flex>
+
+          <ButtonGroup spacing="4">
+            <IconButton>
+              <MdAttachFile />
+            </IconButton>
+
+            <IconButton>
+              <BiDotsVerticalRounded />
+            </IconButton>
+          </ButtonGroup>
+        </Flex>
+
+        <Box height="65vh" overflowY="scroll"></Box>
+
+        <Flex padding={4} borderTop="1px" borderColor="accent.1">
+          <IconButton backgroundColor="accent.1">
+            <AiOutlinePlus />
+          </IconButton>
+
+          <Input placeholder="Type a     message here" variant="ghost" />
+
+          <IconButton shadow="none" marginRight={2}>
+            <GrEmoji />
+          </IconButton>
+
+          <IconButton primary>
+            <IoIosSend />
+          </IconButton>
+        </Flex>
       </Box>
     </Grid>
   );
 };
+
+const IconButton = ({ children, primary, ...rest }) => (
+  <Button
+    {...(primary ? { backgroundColor: "primary.base", color: "white" } : {})}
+    asIcon
+    shadow="md"
+    {...rest}
+  >
+    {children}
+  </Button>
+);
 
 export const ChatLayoutRoute = ({ ...rest }) => {
   return <Route {...rest} render={(props) => <ChatLayout {...props} />} />;
