e9ca6eeff8825472375a0656288552ed6aa7410f
dominic-amalu <dominique.amalu@gmail.com>
2023-03-23 16:55:07 +0100

    master

--- src/index.js
+++ src/index.js
@@ -13,6 +13,7 @@ ReactDOM.render(
   </React.StrictMode>,
   document.getElementById('root')
 );
+// test('should first', () => { second })
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
