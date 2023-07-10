6c20032382f38c1878164804f8279a9747b908d3
lloydpop <lloydperfect199@gmail.com>
2023-07-10 08:48:50 +0100

    updated

--- src/pages/admin/users/UserInfoPage/pages/certTemp.js
+++ src/pages/admin/users/UserInfoPage/pages/certTemp.js
@@ -1,5 +1,7 @@
 import { useRef } from "react";
-import template from "../../../../../assets/images/grooming-cert.png";
+import logo from "../../../../../assets/images/newlogo.png";
+import badge from "../../../../../assets/images/badge-level-1.png";
+import tr from "../../../../../assets/images/tr.png";
 import "./style.css";
 import { AdminMainAreaWrapper } from "../../../../../layouts";
 import { Button } from "../../../../../components";
@@ -7,56 +9,116 @@ import { Box, Flex } from "@chakra-ui/react";
 import { truncateText } from "../../../../../utils";
 import { exportAsImage } from "../../../../../utils/exportToPng";
 
-export const Certificate = ({ name, title, top = "226" }) => {
+export const Certificate = ({ name, title }) => {
   const certificateWrapper = useRef(null);
   return (
     <div>
-      <div className="App">
-        <div id="downloadWrapper" ref={certificateWrapper}>
-          <div id="certificateWrapper">
+      <div className="">
+        <div id="cert" ref={certificateWrapper}>
+          <div id=""></div>
+
+          <div
+            style={{
+              backgroundColor: "#800020",
+              padding: "20px",
+              overflow: "hidden",
+              position: "relative",
+              height: "100%",
+            }}
+          >
+            <div className="design-1"></div>
+            <div className="design-2"></div>
+            <div className="angle"></div>
+            <div className="angle-2"></div>
             <div
               style={{
-                color: "#000",
-                display: "flex",
                 position: "absolute",
-                width: "100%",
-                height: "100%",
-                fontSize: "40px",
-                top: "0",
-                left: "0",
-                fontFamily: "Sacramento",
-                textTransform: "capitalize",
+                bottom: "30px",
+                right: "30px",
               }}
             >
+              <img src={tr} width={"60px"} />
+            </div>
+            <div
+              style={{
+                width: "100%",
+                height: "100%",
+                backgroundColor: "#fff",
+                paddingBottom: "20px",
+              }}
+            >
+              <div className="main-b">
+                <div>
+                  <img src={logo} width={"50px"} />
+                </div>
+                <h1>Grooming Center Training</h1>
+              </div>
+              <div className="main-b">
+                <h1>Certificate of Completion</h1>
+              </div>
               <div
                 style={{
                   textAlign: "center",
-                  top: "226px",
-                  left: "280px",
-                  position: "absolute",
+                  marginTop: "20px",
                 }}
               >
-                {name}
+                <h3>This is to proudly certify that</h3>
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "20px",
+                    fontSize: "30px",
+                    fontFamily: "Sacramento",
+                  }}
+                >
+                  <div className="line-c">
+                    <p>{name}</p>
+                    <p className="line"></p>
+                  </div>
+                </div>
+                <div
+                  style={{
+                    textAlign: "center",
+                    marginTop: "28px",
+                    paddingLeft: "30px",
+                    paddingRight: "30px",
+                  }}
+                >
+                  <h3>
+                    has fullfilled all the neccessary requirements requested for
+                  </h3>
+                  <div
+                    style={{
+                      textAlign: "center",
+                      marginTop: "10px",
+                      fontSize: "30px",
+                      fontFamily: "Sacramento",
+                    }}
+                  >
+                    <div className="line-c">
+                      <p>{truncateText(title, 20)}</p>
+                    </div>
+                  </div>
+                  <div
+                    style={{
+                      marginTop: "30px",
+                    }}
+                  >
+                    <img
+                      src={badge}
+                      width={"70px"}
+                      style={{
+                        margin: "auto",
+                      }}
+                    />
+                  </div>
+                </div>
               </div>
-              <p
-                color="others.3"
-                style={{
-                  display: "flex",
-                  position: "absolute",
-                  top: "370px",
-                  left: title?.length < 20 ? "40%" : "30%",
-                  fontFamily: "Sacramento",
-                  fontSize: "20px",
-                }}
-              >
-                {truncateText(title, 20)}
-              </p>
             </div>
-            <img src={template} alt="Certificate" />
           </div>
         </div>
       </div>
-      <Flex justifyContent={"flex-end"} mr={6}>
+      <Flex justifyContent={"flex-end"} mt={6}>
         <Button
           onClick={() => {
             exportAsImage(
@@ -65,7 +127,7 @@ export const Certificate = ({ name, title, top = "226" }) => {
             );
           }}
         >
-          Download
+          Export
         </Button>
       </Flex>
     </div>
