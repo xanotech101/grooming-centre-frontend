7f1af3dbb6bc5dabacb636248c223ab1d1344a05
lloydpop <lloydperfect199@gmail.com>
2023-11-22 14:35:57 +0100

    updated

--- src/layouts/user/Header/NavBar.jsx
+++ src/layouts/user/Header/NavBar.jsx
@@ -1,42 +1,44 @@
-import { Flex } from '@chakra-ui/layout';
-import { Link, Text } from '../../../components';
+import { Flex } from "@chakra-ui/layout";
+import { Link, Text } from "../../../components";
+import { useApp } from "../../../contexts";
 
 const links = [
   {
-    href: '/dashboard',
-    text: 'Dashboard',
+    href: "/dashboard",
+    text: "Dashboard",
   },
   {
-    href: '/courses',
-    text: 'Courses',
+    href: "/courses",
+    text: "Courses",
   },
   {
-    href: '/library/books',
-    text: 'Library',
+    href: "/library/books",
+    text: "Library",
   },
   {
-    href: '/forum',
-    text: 'Forum',
+    href: "/forum",
+    text: "Forum",
   },
   {
-    href: '/events',
-    text: 'Events',
+    href: "/events",
+    text: "Events",
   },
   {
-    href: '/polls',
-    text: 'Polls',
+    href: "/polls",
+    text: "Polls",
   },
   {
-    href: '/courses/grade-overview',
-    text: 'Grades',
+    href: "/courses/grade-overview",
+    text: "Grades",
   },
   {
-    href: '/standalone-exams',
-    text: 'Exams',
+    href: "/standalone-exams",
+    text: "Exams",
   },
 ];
 
 const NavBar = ({ ...rest }) => {
+  const { handleLogout } = useApp();
   return (
     <Flex as="nav" alignSelf="stretch" {...rest}>
       <Flex listStyleType="none" as="ul">
@@ -52,6 +54,17 @@ const NavBar = ({ ...rest }) => {
             </Link>
           </li>
         ))}
+        <Text
+          as="level3"
+          color="red"
+          cursor="pointer"
+          ml={4}
+          onClick={handleLogout}
+          alignSelf="center"
+          font="bold"
+        >
+          Logout
+        </Text>
       </Flex>
     </Flex>
   );
